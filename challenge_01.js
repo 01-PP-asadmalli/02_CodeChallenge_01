45462fece9ac12a8aa8d2aec5b8de54a2ef7be76
asadmalli <asadmaqsoodmalli@gmail.com>
2023-05-09 15:26:17 +0200

    Taschenrechner

--- challenge_01.js
+++ challenge_01.js
@@ -9,7 +9,43 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+// var zahl1 = prompt("Bitte gib mir die ertse Zahl ein:");
+// var zahl2 = prompt("Bitte gib mir die Zweite Zahl ein:");
+
+// console.log(zahl1+zahl2);
+
+// let zahl1 = prompt("Bitte gib mir die ertse Zahl ein:");
+// let zahl2 = prompt("Bitte gib mir die ertse Zahl zwei:");
+
+// zahl1 = parseInt (zahl1);
+// zahl2 = parseInt (zahl2);
+
+// console.log(zahl1+zahl2);
+
+
+
 let zahl1 = prompt("Bitte gib mir die ertse Zahl ein:");
-let zahl2 = prompt("Bitte gib mir die Zweite Zahl ein:");
-let summe = zahl1 + zahl2;
-console.log(summe);
+let zahl2 = prompt("Bitte gib mir die ertse Zahl zwei:");
+let operation = prompt("Welche Rechenoperation möchtest du durchführen? (+, -, *, /)");
+
+
+zahl1 = parseFloat(zahl1);
+zahl2 = parseFloat(zahl2);
+
+
+
+let result;
+if (operation === "+") {
+  result = zahl1 + zahl2;
+} else if (operation === "-") {
+  result = zahl1 - zahl2;
+} else if (operation === "*") {
+  result = zahl1 * zahl2;
+} else if (operation === "/") {
+  result = zahl1 / zahl2;
+} else {
+  console.log("Ungültige Eingabe");
+}
+
+
+console.log("Das Ergebnis ist: " + result);
